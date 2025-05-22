f061a5cd6b8153d0687da3c7527f04c90ce95b64
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2025-05-22 16:45:17 +0500

    code update

---
+++ app.js
@@ -0,0 +1,18 @@
+const watchBtn = document.getElementById("watch-btn")
+
+const closeIcon = document.querySelector(".close-icon")
+
+const trailerContainer = document.querySelector(".video-container")
+
+const videoEle = document.querySelector("video")
+
+watchBtn.addEventListener("click" , () => {
+    trailerContainer.classList.remove("active")
+    
+})
+
+closeIcon.addEventListener("click" , () =>{
+    trailerContainer.classList.add("active") 
+    videoEle.pause()
+    videoEle.currentTime = 0
+})
